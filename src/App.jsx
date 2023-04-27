45ce791465a54bb347f041d6e90fcc194827a27a
Anush79 <jaiswalkrituraj20@gmail.com>
2023-04-27 13:21:44 +0530

    feat:added Router for each screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+
+import Archive from "./screens/Archive";
+import Labels from "./screens/Labels";
+import Notes from "./screens/Notes";
+import Reminders from "./screens/Reminders";
+import Trash from "./screens/Trash";
+
 function App() {
-  return <div className="App">Hello Neha</div>;
+  return (
+    <div className="App">
+      Welcome to the ToDo App
+     
+      <Routes>
+        <Route path="/archives" element={<Archive/>}></Route>
+        <Route path="/labels" element={<Labels/>}></Route>
+        <Route path="/notes" element={<Notes/>}></Route>
+        <Route path="/reminders" element={<Reminders/>}></Route>
+        <Route path="/trash" element={<Trash/>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
