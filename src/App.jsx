a808f13bebd4b722940fc55cedc7fc3f80ee2f0a
Pranita <pfulsundar8@gmail.com>
2023-04-29 05:24:06 +0530

    add: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 
 import { Archive, Labels, Notes, Reminders, Trash } from "./screens";
+import Header from "./Layout/Header";
 
 function App() {
   return (
     <div className="App">
+    <Header />
       Welcome to the ToDo App
       <Routes>
         <Route path="/archives" element={<Archive />}></Route>
--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import './Header.scss';
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import ViewAgendaOutlinedIcon from "@mui/icons-material/ViewAgendaOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 
 function Header() {
-  return <div></div>;
+  return (
+    <div className='navbar'>
+      <div className='nav-left'>
+        <MenuOutlinedIcon className='hamburger'/>
+        <img
+          src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
+          alt="logo"
+          className='logo-img'
+        />
+        <span className='app-name'>Keep</span>
+      </div>
+      <div className='nav-mid'>
+        <SearchOutlinedIcon className='search-icon'/>
+        <input placeholder="Search" />
+      </div>
+      <div className='nav-right'>
+        <GridViewOutlinedIcon className='nav-icon'/>
+        <ViewAgendaOutlinedIcon className='nav-icon' />
+        <DarkModeOutlinedIcon className='nav-icon' />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
